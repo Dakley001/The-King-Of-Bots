c417a83d07d373297f917f7f6af50d0826c6e5ba
elias <g120000@yeah.net>
2024-07-19 10:35:08 +0800

    匹配微服务实现完成

---
+++ backendcloud/backend/src/main/java/com/kob/backend/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.kob.backend.config;
+
+import com.kob.backend.config.filter.JwtAuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+// 放行登录、注册等接口
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final IpAddressMatcher ipAddressMatcher = new IpAddressMatcher("127.0.0.1");
+
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(CsrfConfigurer::disable) // 基于token，不需要csrf
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 基于token，不需要session
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(new AntPathRequestMatcher("/user/account/token/"), new AntPathRequestMatcher("/user/account/register/")).permitAll() // 放行api
+                        .requestMatchers(new AntPathRequestMatcher("/pk/start/game/"))
+                        .access((authentication, context) -> {
+                            boolean matches = ipAddressMatcher.matches(context.getRequest());
+                            return new AuthorizationDecision(matches);
+                        })
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/websocket/**");
+//    }
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+            return (web) -> web.ignoring().requestMatchers("/websocket/**");
+        }
+}
---
+++ backendcloud/matchingsystem/src/main/java/com/kob/matchingsystem/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.kob.matchingsystem.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final IpAddressMatcher ipAddressMatcher = new IpAddressMatcher("127.0.0.1");
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(CsrfConfigurer::disable) // 基于token，不需要csrf
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 基于token，不需要session
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(new AntPathRequestMatcher("/player/add/"), new AntPathRequestMatcher("/player/remove/"))
+                        .access((authentication, context) -> {
+                            boolean matches = ipAddressMatcher.matches(context.getRequest());
+                            return new AuthorizationDecision(matches);
+                        })// 放行api
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        System.out.println("test");
+        return http.build();
+    }
+}
